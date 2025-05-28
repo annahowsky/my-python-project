555b54e79fe21b5eabe3f5c033464d2ee32c9374
Anna Wehowsky~ <annahowsky@gmail.com>
2025-05-27 20:14:45 -0700

    Intial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+#print('Anna Wehowsky') # print string in the ''
+#print('o----')
+#print(' ||||')
+#print('*' * 10) # this is called an expression - it produces a value
+#####
+#####
+#####
+# variables
+#price = 10 # integer
+#print(price) # we are printing the value of the variable
+#rating = 4.9 # float number
+# name = 'Anna' # string
+#is_published = False # boolean = True or False # also this is case-sensitive
+# practice using variables
+#patient_name = 'John Smith'
+#age = 20
+#new_patient = True
+#name = input('What is your name? ') # naming a new variable using an expression
+#color = input('What is your favorite color? ') # input expression asks for user input.
+#print('Hi ' + name + '!') # concatenate
+#print(name+ ' likes ' + color + '.')
+#####
+#####
+#####
+#birth_year = input('What is your birth year? ') # anything typed as a response to this is always treated as a string
+#print(type(birth_year))
+#age = 2025 - int(birth_year)
+#print(type(age))
+#print(age)
+weight_lbs = input("Enter your weight in lbs: ")
+weight_kg = int(weight_lbs) * 0.45359237
+print(weight_kg, 'kg ')
+#####
+#####
+#####
+
+C:\Users\annah\PycharmProjects\HelloWorld
