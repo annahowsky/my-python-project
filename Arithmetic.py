1bec9e3a3cdfdff9b1c42b28118da8f751f77558
Anna Wehowsky~ <annahowsky@gmail.com>
2025-05-29 16:55:24 -0700

    Next tutorial on Strings - methods.

---
+++ Arithmetic.py
@@ -0,0 +1,24 @@
+#####
+# integers and floats
+# Arithmetic operations
+#print(10 + 3)
+#print(10 / 3) # floating point number
+#print(10 // 3) # will return whole integer
+#print(10 % 3) # will return remainder (modulus)
+#print(10 ** 3) # exponent (power)
+# augmented assignment operator
+#x = 10
+#x = x + 3
+#x +=3 # augmented (enhanced) assignment operator, we are incrementing
+#print(x)
+#x = 10 + 3 * 2 ** 2 # operator precedence (order of operations)
+#print(x)
+x = -2.9
+print(round(x))
+print(abs(x)) #some of the built-in functions for math
+# you need a math module in Python which contains reusable functions for performing mathematical functions
+import math
+# now math is an object - we can access the functions with dot.
+print(math.ceil(2.9))
+print(math.floor(2.9))
+# search for Python math module - this will show us all the math methods that are available!
